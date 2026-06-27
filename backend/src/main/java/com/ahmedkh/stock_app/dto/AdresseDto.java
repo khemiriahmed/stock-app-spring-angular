93c21d5bc0606bb3f55b332484ecd5145ffc7a80
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-27 21:49:26 +0100

    AdresseDto

--- backend/src/main/java/com/ahmedkh/stock_app/dto/AdresseDto.java
+++ backend/src/main/java/com/ahmedkh/stock_app/dto/AdresseDto.java
@@ -1,6 +1,7 @@
 package com.ahmedkh.stock_app.dto;
 
 
+import com.ahmedkh.stock_app.model.Adresse;
 import lombok.Builder;
 import lombok.Data;
 
@@ -20,4 +21,32 @@ public class AdresseDto {
     private String codePostale;
 
     private String pays;
+
+    public AdresseDto fromEntity(Adresse adresse) {
+        if (adresse == null) {
+            return null;
+        }
+
+        return AdresseDto.builder()
+                .adresse1(adresse.getAdresse1())
+                .adresse2(adresse.getAdresse2())
+                .codePostale(adresse.getCodePostale())
+                .ville(adresse.getVille())
+                .pays(adresse.getPays())
+                .build();
+    }
+
+    public Adresse toEntity(AdresseDto adresseDto){
+        if(adresseDto == null) {
+            return null;
+        }
+        Adresse adresse = new Adresse();
+        adresse.setAdresse1(adresseDto.getAdresse1());
+        adresse.setAdresse2(adresse.getAdresse2());
+        adresse.setCodePostale(adresseDto.getCodePostale());
+        adresse.setVille(adresseDto.getVille());
+        adresse.setPays(adresse.getPays());
+        return adresse;
+    }
+
 }
