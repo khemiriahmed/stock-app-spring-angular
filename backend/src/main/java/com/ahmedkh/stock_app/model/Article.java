c5f01295347b195a932ca5279827d7bb2cb6ceb4
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-28 11:28:24 +0100

    Model

--- backend/src/main/java/com/ahmedkh/stock_app/model/Article.java
+++ backend/src/main/java/com/ahmedkh/stock_app/model/Article.java
@@ -5,9 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
 @Data
