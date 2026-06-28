53f2169b260239b2cea52826c8d226c9c7f9b71d
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-28 22:21:28 +0100

    Handlers

---
+++ backend/src/main/java/com/ahmedkh/stock_app/handlers/ErrorDto.java
@@ -0,0 +1,28 @@
+package com.ahmedkh.stock_app.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ahmedkh.stock_app.exception.ErrorCodes;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private Integer httpCode;
+
+    private ErrorCodes code;
+
+    private String message;
+
+    private List<String> errors = new ArrayList<>();
+
+}
---
+++ backend/src/main/java/com/ahmedkh/stock_app/handlers/RestExceptionHandler.java
@@ -0,0 +1,74 @@
+package com.ahmedkh.stock_app.handlers;
+
+import com.ahmedkh.stock_app.exception.EntityNotFoundException;
+import com.ahmedkh.stock_app.exception.ErrorCodes;
+import com.ahmedkh.stock_app.exception.InvalidEntityException;
+import com.ahmedkh.stock_app.exception.InvalidOperationException;
+import java.util.Collections;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleException(EntityNotFoundException exception, WebRequest webRequest) {
+
+        final HttpStatus notFound = HttpStatus.NOT_FOUND;
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(notFound.value())
+                .message(exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorDto, notFound);
+    }
+
+    @ExceptionHandler(InvalidOperationException.class)
+    public ResponseEntity<ErrorDto> handleException(InvalidOperationException exception, WebRequest webRequest) {
+
+        final HttpStatus notFound = HttpStatus.BAD_REQUEST;
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(notFound.value())
+                .message(exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorDto, notFound);
+    }
+
+    @ExceptionHandler(InvalidEntityException.class)
+    public ResponseEntity<ErrorDto> handleException(InvalidEntityException exception, WebRequest webRequest) {
+        final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(badRequest.value())
+                .message(exception.getMessage())
+                .errors(exception.getErrors())
+                .build();
+
+        return new ResponseEntity<>(errorDto, badRequest);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorDto> handleException(BadCredentialsException exception, WebRequest webRequest) {
+        final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(ErrorCodes.BAD_CREDENTIALS)
+                .httpCode(badRequest.value())
+                .message(exception.getMessage())
+                .errors(Collections.singletonList("Login et / ou mot de passe incorrecte"))
+                .build();
+
+        return new ResponseEntity<>(errorDto, badRequest);
+    }
+
+}
