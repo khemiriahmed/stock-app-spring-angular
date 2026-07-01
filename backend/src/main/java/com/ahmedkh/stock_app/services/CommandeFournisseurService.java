cf23efb06573af6b1aa169e13e79fcd762cd0114
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-01 12:00:13 +0100

    CommandeFournisseur

---
+++ backend/src/main/java/com/ahmedkh/stock_app/services/CommandeFournisseurService.java
@@ -0,0 +1,18 @@
+package com.ahmedkh.stock_app.services;
+
+import com.ahmedkh.stock_app.dto.CommandeFournisseurDto;
+
+import java.util.List;
+
+public interface CommandeFournisseurService {
+
+    CommandeFournisseurDto save(CommandeFournisseurDto dto);
+
+    CommandeFournisseurDto findById(Integer id);
+
+    CommandeFournisseurDto findByCode(String code);
+
+    List<CommandeFournisseurDto> findAll();
+
+    void delete(Integer id);
+}
