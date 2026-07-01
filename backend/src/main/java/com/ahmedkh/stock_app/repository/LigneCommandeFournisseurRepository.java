32e4b92a64f2dbc2079ef95b7f2eabae054debaa
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-01 12:01:13 +0100

    LigneCommandeFournisseurRepository

--- backend/src/main/java/com/ahmedkh/stock_app/repository/LigneCommandeFournisseurRepository.java
+++ backend/src/main/java/com/ahmedkh/stock_app/repository/LigneCommandeFournisseurRepository.java
@@ -1,12 +1,11 @@
 package com.ahmedkh.stock_app.repository;
 
-import com.ahmedkh.stock_app.model.LigneCommandeClient;
 import com.ahmedkh.stock_app.model.LigneCommandeFournisseur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface LigneCommmandeFournisseurRepository extends JpaRepository<LigneCommandeFournisseur , Integer> {
+public interface LigneCommandeFournisseurRepository  extends JpaRepository<LigneCommandeFournisseur , Integer> {
 
     List<LigneCommandeFournisseur> findAllByCommandeFournisseurId(Integer idCommande);
 
