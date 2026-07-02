b607bdb3abac8362a199e495ff34bbd574562c38
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-02 15:56:06 +0100

    Controller

---
+++ backend/src/main/java/com/ahmedkh/stock_app/controller/ClientController.java
@@ -0,0 +1,41 @@
+package com.ahmedkh.stock_app.controller;
+
+import com.ahmedkh.stock_app.controller.api.ClientApi;
+import com.ahmedkh.stock_app.dto.ClientDto;
+import com.ahmedkh.stock_app.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ClientController implements ClientApi {
+
+    private ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+
+    @Override
+    public ClientDto save(ClientDto dto) {
+        return clientService.save(dto);
+    }
+
+    @Override
+    public ClientDto findById(Integer id) {
+        return clientService.findById(id);
+    }
+
+    @Override
+    public List<ClientDto> findAll() {
+        return clientService.findAll();
+    }
+
+    @Override
+    public void delete(Integer id) {
+        clientService.delete(id);
+    }
+}
