36af64b68deb842157000b20ccb52d2869c11785
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-05 16:40:04 +0100

    Controller

--- backend/src/main/java/com/ahmedkh/stock_app/controller/CommandeClientController.java
+++ backend/src/main/java/com/ahmedkh/stock_app/controller/CommandeClientController.java
@@ -2,12 +2,15 @@ package com.ahmedkh.stock_app.controller;
 
 import com.ahmedkh.stock_app.controller.api.CommandeClientApi;
 import com.ahmedkh.stock_app.dto.CommandeClientDto;
+import com.ahmedkh.stock_app.dto.LigneCommandeClientDto;
+import com.ahmedkh.stock_app.model.EtatCommande;
 import com.ahmedkh.stock_app.services.CommandeClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -22,8 +25,32 @@ public class CommandeClientController implements CommandeClientApi {
 
     @Override
     public ResponseEntity<CommandeClientDto> save(CommandeClientDto dto) {
-        //return ResponseEntity.ok(commandeClientService.save(dto));
-        return ResponseEntity.status(HttpStatus.OK).body(commandeClientService.save(dto));
+        return ResponseEntity.ok(commandeClientService.save(dto));
+    }
+
+    @Override
+    public ResponseEntity<CommandeClientDto> updateEtatCommande(Integer idCommande, EtatCommande etatCommande) {
+        return ResponseEntity.ok(commandeClientService.updateEtatCommande(idCommande, etatCommande));
+    }
+
+    @Override
+    public ResponseEntity<CommandeClientDto> updateQuantiteCommande(Integer idCommande, Integer idLigneCommande, BigDecimal quantite) {
+        return ResponseEntity.ok(commandeClientService.updateQuantiteCommande(idCommande, idLigneCommande, quantite));
+    }
+
+    @Override
+    public ResponseEntity<CommandeClientDto> updateClient(Integer idCommande, Integer idClient) {
+        return ResponseEntity.ok(commandeClientService.updateClient(idCommande, idClient));
+    }
+
+    @Override
+    public ResponseEntity<CommandeClientDto> updateArticle(Integer idCommande, Integer idLigneCommande, Integer idArticle) {
+        return ResponseEntity.ok(commandeClientService.updateArticle(idCommande, idLigneCommande, idArticle));
+    }
+
+    @Override
+    public ResponseEntity<CommandeClientDto> deleteArticle(Integer idCommande, Integer idLigneCommande) {
+        return ResponseEntity.ok(commandeClientService.deleteArticle(idCommande, idLigneCommande));
     }
 
     @Override
@@ -42,7 +69,12 @@ public class CommandeClientController implements CommandeClientApi {
     }
 
     @Override
-    public ResponseEntity delete(Integer id) {
+    public ResponseEntity<List<LigneCommandeClientDto>> findAllLignesCommandesClientByCommandeClientId(Integer idCommande) {
+        return ResponseEntity.ok(commandeClientService.findAllLignesCommandesClientByCommandeClientId(idCommande));
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Integer id) {
         commandeClientService.delete(id);
         return ResponseEntity.ok().build();
     }
--- backend/src/main/java/com/ahmedkh/stock_app/controller/api/CommandeClientApi.java
+++ backend/src/main/java/com/ahmedkh/stock_app/controller/api/CommandeClientApi.java
@@ -1,28 +1,50 @@
 package com.ahmedkh.stock_app.controller.api;
 
 import com.ahmedkh.stock_app.dto.CommandeClientDto;
+import com.ahmedkh.stock_app.dto.LigneCommandeClientDto;
+import com.ahmedkh.stock_app.model.EtatCommande;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static com.ahmedkh.stock_app.utils.Constants.APP_ROOT;
-@Tag(name = "Commande Client", description = APP_ROOT)
-public interface CommandeClientApi {
 
+
+public interface CommandeClientApi {
     @PostMapping(APP_ROOT + "/commandesclients/create")
     ResponseEntity<CommandeClientDto> save(@RequestBody CommandeClientDto dto);
 
-    @GetMapping(APP_ROOT + "/commandesclients/filter/{codeCommandeClient}")
-    ResponseEntity<CommandeClientDto> findByCode(@PathVariable("codeCommandeClient") String code);
+    @PatchMapping(APP_ROOT + "/commandesclients/update/etat/{idCommande}/{etatCommande}")
+    ResponseEntity<CommandeClientDto> updateEtatCommande(@PathVariable("idCommande") Integer idCommande, @PathVariable("etatCommande") EtatCommande etatCommande);
 
+    @PatchMapping(APP_ROOT + "/commandesclients/update/quantite/{idCommande}/{idLigneCommande}/{quantite}")
+    ResponseEntity<CommandeClientDto> updateQuantiteCommande(@PathVariable("idCommande") Integer idCommande,
+                                                             @PathVariable("idLigneCommande") Integer idLigneCommande, @PathVariable("quantite") BigDecimal quantite);
+
+    @PatchMapping(APP_ROOT + "/commandesclients/update/client/{idCommande}/{idClient}")
+    ResponseEntity<CommandeClientDto> updateClient(@PathVariable("idCommande") Integer idCommande, @PathVariable("idClient") Integer idClient);
+
+    @PatchMapping(APP_ROOT + "/commandesclients/update/article/{idCommande}/{idLigneCommande}/{idArticle}")
+    ResponseEntity<CommandeClientDto> updateArticle(@PathVariable("idCommande") Integer idCommande,
+                                                    @PathVariable("idLigneCommande") Integer idLigneCommande, @PathVariable("idArticle") Integer idArticle);
+
+    @DeleteMapping(APP_ROOT + "/commandesclients/delete/article/{idCommande}/{idLigneCommande}")
+    ResponseEntity<CommandeClientDto> deleteArticle(@PathVariable("idCommande") Integer idCommande, @PathVariable("idLigneCommande") Integer idLigneCommande);
 
     @GetMapping(APP_ROOT + "/commandesclients/{idCommandeClient}")
     ResponseEntity<CommandeClientDto> findById(@PathVariable Integer idCommandeClient);
 
+    @GetMapping(APP_ROOT + "/commandesclients/filter/{codeCommandeClient}")
+    ResponseEntity<CommandeClientDto> findByCode(@PathVariable("codeCommandeClient") String code);
+
     @GetMapping(APP_ROOT + "/commandesclients/all")
     ResponseEntity<List<CommandeClientDto>> findAll();
+
+    @GetMapping(APP_ROOT + "/commandesclients/lignesCommande/{idCommande}")
+    ResponseEntity<List<LigneCommandeClientDto>> findAllLignesCommandesClientByCommandeClientId(@PathVariable("idCommande") Integer idCommande);
 
     @DeleteMapping(APP_ROOT + "/commandesclients/delete/{idCommandeClient}")
     ResponseEntity<Void> delete(@PathVariable("idCommandeClient") Integer id);
