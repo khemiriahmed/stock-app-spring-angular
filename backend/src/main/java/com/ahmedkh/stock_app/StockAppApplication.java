8df644ff13f31ceeb12d7a108552727691158180
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-05 10:02:36 +0100

    StockApplication

--- backend/src/main/java/com/ahmedkh/stock_app/StockAppApplication.java
+++ backend/src/main/java/com/ahmedkh/stock_app/StockAppApplication.java
@@ -1,11 +1,13 @@
 package com.ahmedkh.stock_app;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Configurable
 public class StockAppApplication {
 
 	public static void main(String[] args) {
