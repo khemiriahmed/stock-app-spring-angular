568199283460e6f64e59b2725174c0b5d00a7dbd
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-06 10:45:24 +0100

    Services

--- backend/src/main/java/com/ahmedkh/stock_app/services/ArticleService.java
+++ backend/src/main/java/com/ahmedkh/stock_app/services/ArticleService.java
@@ -1,6 +1,9 @@
 package com.ahmedkh.stock_app.services;
 
 import com.ahmedkh.stock_app.dto.ArticleDto;
+import com.ahmedkh.stock_app.dto.LigneCommandeClientDto;
+import com.ahmedkh.stock_app.dto.LigneCommandeFournisseurDto;
+import com.ahmedkh.stock_app.dto.LigneVenteDto;
 
 import java.util.List;
 
@@ -13,6 +16,14 @@ public interface ArticleService {
   ArticleDto findByCodeArticle(String codeArticle);
 
   List<ArticleDto> findAll();
+
+  List<LigneVenteDto> findHistoriqueVentes(Integer idArticle);
+
+  List<LigneCommandeClientDto> findHistoriaueCommandeClient(Integer idArticle);
+
+  List<LigneCommandeFournisseurDto> findHistoriqueCommandeFournisseur(Integer idArticle);
+
+  List<ArticleDto> findAllArticleByIdCategory(Integer idCategory);
 
   void delete(Integer id);
 
