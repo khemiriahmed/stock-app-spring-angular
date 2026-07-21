30e9e04b58d2f1d26770254b829239a13489ee88
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-21 15:09:22 +0100

    page login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
 @Component({
   selector: 'app-root',
+   standalone: true,
+  imports: [PageLoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
