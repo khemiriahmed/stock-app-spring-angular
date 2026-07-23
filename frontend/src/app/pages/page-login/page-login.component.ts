a29a6fef55ab4bf39b09e1276524fbd5f83e91ac
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-23 17:58:47 +0100

    page login

--- frontend/src/app/pages/page-login/page-login.component.ts
+++ frontend/src/app/pages/page-login/page-login.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-page-login',
+  standalone: true,
+   imports: [RouterLink],
   templateUrl: './page-login.component.html',
   styleUrls: ['./page-login.component.scss']
 })
