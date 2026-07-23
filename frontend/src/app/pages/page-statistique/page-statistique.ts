56d2dcac24ee3bed5c78f34111c7f7759f5c98c1
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-23 17:49:36 +0100

    page statistique

---
+++ frontend/src/app/pages/page-statistique/page-statistique.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-statistique',
+  imports: [],
+  templateUrl: './page-statistique.html',
+  styleUrl: './page-statistique.scss',
+})
+export class PageStatistique {
+
+}
