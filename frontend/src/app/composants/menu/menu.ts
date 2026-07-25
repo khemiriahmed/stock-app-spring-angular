4baef83399936039d357cee3922a3dda74dcf685
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-25 16:04:15 +0100

    Menu

---
+++ frontend/src/app/composants/menu/menu.ts
@@ -0,0 +1,124 @@
+import { Component, OnInit } from '@angular/core';
+import { Menu } from './menu-sidebar';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+   standalone: true, 
+   imports: [RouterLink],
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss',
+})
+export class MenuComponent implements OnInit {
+  public menuProperties: Array<Menu> = [
+    {
+      id: '1',
+      titre: 'tableau de board',
+      icon: '',
+      url: '',
+      sousMenu: [
+        {
+          id: '11',
+          titre: "Vue d'ensemble",
+          icon: 'fas fa-chart-pie',
+          url: '',
+        },
+        {
+          id: '12',
+          titre: 'Statistiques',
+          icon: 'fas fa-chart-bar',
+          url: 'statistiques',
+        },
+      ],
+    },
+
+   {
+      id: '2',
+      titre: 'Articles',
+      icon: 'fas fa-boxes',
+      url: '',
+      sousMenu: [
+        {
+          id: '21',
+          titre: 'Articles',
+          icon: 'fas fa-boxes',
+          url: 'articles'
+        },
+        {
+          id: '22',
+          titre: 'Mouvements du stock',
+          icon: 'fab fa-stack-overflow',
+          url: 'mvtstk'
+        }
+      ]
+    },
+
+        {
+      id: '3',
+      titre: 'Clients',
+      icon: 'fas fa-users',
+      url: '',
+      sousMenu: [
+        {
+          id: '31',
+          titre: 'Clients',
+          icon: 'fas fa-users',
+          url: 'clients'
+        },
+        {
+          id: '32',
+          titre: 'Commandes clients',
+          icon: 'fas fa-shopping-basket',
+          url: 'commandesclient'
+        }
+      ]
+    },
+
+     {
+      id: '4',
+      titre: 'Fournisseurs',
+      icon: 'fas fa-users',
+      url: '',
+      sousMenu: [
+        {
+          id: '41',
+          titre: 'Fournisseurs',
+          icon: 'fas fa-users',
+          url: 'fournisseurs'
+        },
+        {
+          id: '42',
+          titre: 'Commandes fournisseurs',
+          icon: 'fas fa-truck',
+          url: 'commandesfournisseur'
+        }
+      ]
+    },
+
+    {
+      id: '5',
+      titre: 'Parametrages',
+      icon: 'fas fa-cogs',
+      url: '',
+      sousMenu: [
+        {
+          id: '51',
+          titre: 'Categories',
+          icon: 'fas fa-tools',
+          url: 'categories'
+        },
+        {
+          id: '52',
+          titre: 'Utilisateurs',
+          icon: 'fas fa-users-cog',
+          url: 'utilisateurs'
+        }
+      ]
+    }
+
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
