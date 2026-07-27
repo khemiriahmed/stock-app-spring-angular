fab56f4af84d2a894d86636dc1ee5043d7a26ef1
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-27 20:28:54 +0100

    dashboard

--- frontend/src/app/pages/page-dashboard/page-dashboard.ts
+++ frontend/src/app/pages/page-dashboard/page-dashboard.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MenuComponent  } from '../../composants/menu/menu';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from '../../composants/menu/menu';
 @Component({
   selector: 'app-page-dashboard',
-    standalone: true,
- imports: [MenuComponent ],
+  standalone: true,
+  imports: [MenuComponent, RouterOutlet],
   templateUrl: './page-dashboard.html',
   styleUrl: './page-dashboard.scss',
 })
-export class PageDashboard {
-
-}
+export class PageDashboard {}
