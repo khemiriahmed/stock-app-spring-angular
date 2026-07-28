d6d5ee18020aab25ab489a266ee2aa7f1795b000
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-28 20:47:51 +0100

    page article add pagination,

--- frontend/src/app/pages/articles/page-article/page-article.ts
+++ frontend/src/app/pages/articles/page-article/page-article.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DetailArticle } from 'src/app/composants/detail-article/detail-article';
+import { Pagination } from 'src/app/composants/pagination/pagination';
 
 @Component({
   selector: 'app-page-article',
-  imports: [DetailArticle],
+  imports: [DetailArticle,Pagination],
   templateUrl: './page-article.html',
   styleUrl: './page-article.scss',
 })
