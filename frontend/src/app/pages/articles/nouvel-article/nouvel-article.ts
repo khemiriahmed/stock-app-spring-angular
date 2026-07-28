5d0dba22ca9f2a7699647f778f26893e15b3bc23
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-28 21:27:19 +0100

    nouvel article

---
+++ frontend/src/app/pages/articles/nouvel-article/nouvel-article.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nouvel-article',
+  imports: [],
+  templateUrl: './nouvel-article.html',
+  styleUrl: './nouvel-article.scss',
+})
+export class NouvelArticle {
+
+}
