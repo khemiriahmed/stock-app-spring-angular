577bf302d372c7734e3bc6be2f02d2148798d07b
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-28 21:01:16 +0100

    page article

--- frontend/src/app/pages/articles/page-article/page-article.ts
+++ frontend/src/app/pages/articles/page-article/page-article.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonAction } from 'src/app/composants/button-action/button-action';
 import { DetailArticle } from 'src/app/composants/detail-article/detail-article';
 import { Pagination } from 'src/app/composants/pagination/pagination';
 
 @Component({
   selector: 'app-page-article',
-  imports: [DetailArticle,Pagination],
+  imports: [DetailArticle,Pagination,ButtonAction],
   templateUrl: './page-article.html',
   styleUrl: './page-article.scss',
 })
