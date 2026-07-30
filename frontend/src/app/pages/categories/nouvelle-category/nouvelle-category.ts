078b564a9cc7de968f57fff249efa5918c4b11e9
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-30 19:58:44 +0100

    categorie

---
+++ frontend/src/app/pages/categories/nouvelle-category/nouvelle-category.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nouvelle-category',
+  imports: [],
+  templateUrl: './nouvelle-category.html',
+  styleUrl: './nouvelle-category.scss',
+})
+export class NouvelleCategory {
+
+}
