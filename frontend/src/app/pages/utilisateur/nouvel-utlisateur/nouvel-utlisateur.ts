cae3af4d472acfb9352840574a72b65915d8882e
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-31 11:10:44 +0100

    nouvel-utilisateur

--- frontend/src/app/pages/utilisateur/nouvel-utlisateur/nouvel-utlisateur.ts
+++ frontend/src/app/pages/utilisateur/nouvel-utlisateur/nouvel-utlisateur.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nouvel-utlisateur',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './nouvel-utlisateur.scss',
 })
 export class NouvelUtlisateur {
+
+  constructor(
+    private router: Router
+  ) { }
+  
+  cancel(): void {
+    this.router.navigate(['utilisateurs']);
+  }
 
 }
