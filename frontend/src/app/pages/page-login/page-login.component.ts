701700bf61b0c69494dc99c654af824b36451827
khemiriahmed <khemiriahmed6@gmail.com>
2026-08-01 13:52:13 +0100

    page login

--- frontend/src/app/pages/page-login/page-login.component.ts
+++ frontend/src/app/pages/page-login/page-login.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component,OnInit,inject   } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink ,Router} from '@angular/router';
+import { UserService } from '../../services/user/user';
+import {AuthenticationRequest} from '../../../gs-api/src/models/authentication-request';
 @Component({
   selector: 'app-page-login',
   standalone: true,
-   imports: [RouterLink],
+   imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './page-login.component.html',
   styleUrls: ['./page-login.component.scss']
 })
-export class PageLoginComponent {
+export class PageLoginComponent implements OnInit {
+
+    authenticationRequest: AuthenticationRequest = {};
+    errorMessage = '';
+
+  
+     private userService = inject(UserService);
+  private router = inject(Router);
+  
+
+
+    ngOnInit(): void {
+  }
+
+   login(): void {
+    this.userService.login(this.authenticationRequest).subscribe({
+      next: (data) => {
+        this.userService.setAccessToken(data);
+        this.getUserByEmail();
+        this.router.navigate(['']);
+      },
+      error: () => {
+        this.errorMessage = 'Login et / ou mot de passe incorrecte';
+      }
+    });
+  }
+
+   getUserByEmail(): void {
+    this.userService.getUserByEmail(this.authenticationRequest.login)
+      .subscribe({
+        next: (user) => {
+          this.userService.setConnectedUser(user);
+        }
+      });
+  }
 
 }
